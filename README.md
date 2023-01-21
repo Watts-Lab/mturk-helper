1ae013b742afd7419750c8fc1c6f5dd49eb2f7f9
Ellen Yan <eyan2024@seas.upenn.edu>
2023-01-21 00:58:26 -0500

    v1 browsersync not allowing me to console log i think

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # mturk-helper
 Package for supporting recruitment and payment of participants in multiplayer experiments
+### scripts
+`code` npm run dev
+* this allows seeing the effect of local changes to files in /public folder
+`code` npm run build
+* this cleans all js files in the /public folder and compiles from TS, source maps excluded
+`code` npm run test
+* this runs tests in src/_test_
+### setup
+1. install editorconfig as an extension in VSCode
+2. the .editorconfig file sets the formats for coding
+3. create a config.json in stc/ts folder and fill in the content as follows
+```
+{
+    "access_key": "YOUR_AWS_ACCESS_KEY",
+    "secret_key": "YOUR_AWS_SECRET_KEY"
+}
+```
--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,40 @@
 "use strict";
-console.log("hahah");
+// //Setting up AWS. Docs: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/MTurk.html
+// const config = require('./config.json');
+// const AWS = require("aws-sdk");
+// const region = "us-east-1";
+// //const aws_access_key_id = config;
+// //const aws_secret_access_key = process.env.YOUR_SECRET_KEY;
+// AWS.config = {
+//   accessKeyId: config.access_key,
+//   secretAccessKey: config.secret_key,
+//   region: region,
+//   sslEnabled: "true",
+// };
+// const sandbox = true; // WARNING Setting this to false could costs you money!
+// const endpoint = `https://${
+//   sandbox ? "mturk-requester-sandbox" : "mturk-requester"
+// }.${region}.amazonaws.com`;
+// const mturk = new AWS.MTurk({ endpoint: endpoint });
+// console.log("haihai");
+// //getter command
+// //List HITs (currently running, expired, done, etc)
+// /**
+//  * Returns a list of HITs and their status associated with the current account
+//  *
+//  * @param x - The first input number
+//  * @param y - The second input number
+//  * @returns The arithmetic mean of `x` and `y`
+//  *
+// */
+// mturk.listHITs({}, (err: any, data:any) => {
+//     if (err) console.log(err, err.stack);
+//     else
+//       console.log(
+//         "Active HITs: \n",
+//         data.HITs.filter((h:any) => h.HITStatus == "Assignable" || h.HITStatus == "Unassignable")
+//       ); //not sure if leaving err and data types as any are the best practices
+//       console.log("\nfinished listing.");
+//   });
+console.log("hai");
 //# sourceMappingURL=app.js.map
